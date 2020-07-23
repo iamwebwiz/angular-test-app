70a3acdaeaf02d5ab9f6e9b6621852d09e992c3d
Ezekiel <iamwebwiz@gmail.com>
2020-07-23 23:44:21 +0100

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  title = "angular-todo-app";
+  todos = [];
+
+  addTodo(value) {
+    this.todos.push(value);
+    console.log(this.todos);
+  }
 }
